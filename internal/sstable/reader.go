b164df69a1852100a3ced6e9ec9cf0d74f4c3e04
axv2655 <arnav.vedula@yahoo.com>
2026-03-25 13:37:17 -0500

    feat: finished reader.go for now atleast, now just need to setup everything in db.go then base implementation is done

--- internal/sstable/reader.go
+++ internal/sstable/reader.go
@@ -2,6 +2,7 @@ package sstable
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"os"
@@ -10,6 +11,57 @@ import (
 type SSTable struct {
 	file  *os.File
 	index []IndexEntry
+}
+
+func openSSTable(filepath string) (*SSTable, error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, fmt.Errorf("error opening sstable file %s: %w", filepath, err)
+	}
+
+	stat, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	footerPos := stat.Size() - 16
+
+	buf := make([]byte, 16)
+	_, err = file.ReadAt(buf, footerPos)
+	if err != nil {
+		return nil, err
+	}
+
+	indexOffset := binary.LittleEndian.Uint64(buf[0:8])
+	lenEntry := binary.LittleEndian.Uint64(buf[8:16])
+	_, err = file.Seek(int64(indexOffset), io.SeekStart)
+	if err != nil {
+		return nil, fmt.Errorf("error seeking to index of sstable file %s: %w", filepath, err)
+	}
+	offset := uint64(0)
+
+	entries := make([]IndexEntry, lenEntry)
+	for i := 0; i < int(lenEntry); i++ {
+		lenBuf := make([]byte, 4)
+		_, err = file.ReadAt(lenBuf, int64(indexOffset)+int64(offset))
+		offset += 4
+		keyLen := binary.LittleEndian.Uint32(lenBuf)
+
+		keyBuf := make([]byte, keyLen)
+		_, err = file.ReadAt(keyBuf, int64(indexOffset)+int64(offset))
+		offset += uint64(keyLen)
+
+		offsetBuf := make([]byte, 8)
+		_, err = file.ReadAt(offsetBuf, int64(indexOffset)+int64(offset))
+		offset += 8
+		entryOffset := binary.LittleEndian.Uint64(offsetBuf)
+
+		entries[i] = IndexEntry{key: keyBuf, offSet: int64(entryOffset)}
+	}
+	return &SSTable{
+		file:  file,
+		index: entries,
+	}, nil
 }
 
 func (s *SSTable) getIndex(key []byte) (*IndexEntry, error) {
@@ -37,28 +89,43 @@ func (s *SSTable) getIndex(key []byte) (*IndexEntry, error) {
 }
 
 func (s *SSTable) Get(key []byte) ([]byte, error) {
+	if len(s.index) == 0 {
+		return nil, fmt.Errorf("sstable index is empty")
+	}
 	entry, err := s.getIndex(key)
 	if err != nil {
-		return nil, fmt.Errorf("Error getting data from SSTable: %w", err)
+		return nil, fmt.Errorf("error getting data from SSTable index: %w", err)
 	}
 
 	_, err = s.file.Seek(entry.offSet, io.SeekStart)
 	if err != nil {
-		fmt.Errorf("Error seeking to offset %d in SSTable: %w", entry.offSet, err)
-	}
-	valueLenBytes := make([]byte, 4)
-	valueLen, err := s.file.Read(valueLenBytes)
-	if err != nil && err != io.EOF {
-		fmt.Errorf("Error reading value length from SSTable: %w", err)
+		return nil, fmt.Errorf("error seeking to offset %d in SSTable: %w", entry.offSet, err)
 	}
 
-	valueBytes := make([]byte, valueLen)
-	_, err = s.file.Read(valueBytes)
-	if err != nil && err != io.EOF {
-		fmt.Errorf("Error reading key length from SSTable: %w", err)
+	lengthBuf := make([]byte, 4)
+	if _, err := io.ReadFull(s.file, lengthBuf); err != nil {
+		return nil, fmt.Errorf("error reading key length: %w", err)
 	}
-	if len(valueBytes) != 0 {
-		return valueBytes, nil
+	keyLen := binary.LittleEndian.Uint32(lengthBuf)
+
+	_, err = s.file.Seek(int64(keyLen), io.SeekCurrent)
+	if err != nil {
+		return nil, fmt.Errorf("error skipping key bytes: %w", err)
 	}
-	return nil, fmt.Errorf("Key %s was not found in SSTable", key)
+
+	if _, err := io.ReadFull(s.file, lengthBuf); err != nil {
+		return nil, fmt.Errorf("error reading value length: %w", err)
+	}
+	valLen := binary.LittleEndian.Uint32(lengthBuf)
+
+	valueBytes := make([]byte, valLen)
+	if _, err := io.ReadFull(s.file, valueBytes); err != nil {
+		return nil, fmt.Errorf("error reading value bytes: %w", err)
+	}
+
+	if len(valueBytes) == 0 {
+		return nil, fmt.Errorf("key %s was deleted (tombstone)", key)
+	}
+
+	return valueBytes, nil
 }
